7f30395381479ed1099169795cd289d291ea006e
samkhangithub <berlanjan123@gmail.com>
2023-01-18 14:22:16 +0500

    changes abcd

--- src/footers/Footer.jsx
+++ src/footers/Footer.jsx
@@ -10,56 +10,55 @@ const Footer = () => {
     <>
 
 
-     
-      <footer className="footer-39201 bg-primary text-white">
-      <div className="container py-5">
-        <div className="row">
-          <div className="col-md-4 mb-4 mb-md-0">
-            <h3>OUR SERVICES</h3>
-            <ul className="list-unstyled nav-links">
-              <li><a href="#">Architecture</a></li>
-              <li><a href="#">Project Management</a></li>
-              <li><a href="#">UX</a></li>
-              <li><a href="#">UI</a></li>
-              <li><a href="#">Development</a></li>
-              <li><a href="#">DevOps</a></li>
-              <li><a href="#">QC</a></li>
-              <li><a href="#">Branding</a></li>
-            </ul>
-          </div>
-          <div className="col-md-4 mb-4 mb-md-0">
-            <h3>OUR COMPANY</h3>
-            <ul className="list-unstyled nav-links">
-              <li><a href="#">Our Services</a></li>
-              <li><a href="#">Industries</a></li>
-              <li><a href="#">Blog</a></li>
-              <li><a href="#">About us</a></li>
-              <li><a href="#">Careers</a></li>
-              <li><a href="#">Contact</a></li>
-            </ul>
-          </div>
-         
-          <div className="col-md-4 mb-4 mb-md-0 pt-4 contect-div">
-            <p className="mb-4">Receive monthly updates through our Newsletter</p>
-               <button className="btn-jion">Jion <MdKeyboardArrowRight className="md_icon" /></button>
-			  <div className="col mb-4 mb-md-0 mt-3 d-md-flex pt-4 abs">
-				<div className="col ">
-				<h3>JOIN OUR TEAM</h3>
-				<span>jobs@patagonian.com</span>
-				</div>
-			<div className="col ">
-				<h3>CONTACT US</h3>
-				<span>jobs@patagonian.com</span>
-				</div>
-			</div>
-          </div>
-			
-        </div>
 
-     
-      </div>
-    </footer>
-        {/* <footer className="py-5">
+      <footer className="footer-39201 bg-primary text-white">
+        <div className="container py-5">
+          <div className="row">
+            <div className="col-md-4 mb-4 mb-md-0">
+              <h3>OUR SERVICES</h3>
+              <ul className="list-unstyled nav-links">
+                <li><a href="#">Architecture</a></li>
+                <li><a href="#">Project Management</a></li>
+                <li><a href="#">UX</a></li>
+                <li><a href="#">UI</a></li>
+                <li><a href="#">Development</a></li>
+                <li><a href="#">DevOps</a></li>
+                <li><a href="#">QC</a></li>
+                <li><a href="#">Branding</a></li>
+              </ul>
+            </div>
+            <div className="col-md-4 mb-4 mb-md-0">
+              <h3>OUR COMPANY</h3>
+              <ul className="list-unstyled nav-links">
+                <li><a href="#">Our Services</a></li>
+                <li><a href="#">Industries</a></li>
+                <li><a href="#">Blog</a></li>
+                <li><a href="#">About us</a></li>
+                <li><a href="#">Careers</a></li>
+                <li><a href="#">Contact</a></li>
+              </ul>
+            </div>
+            <div className="col-md-4 mb-4 mb-md-0 pt-4 contect-div">
+              <p className="mb-4">Receive monthly updates through our Newsletter</p>
+              <button className="btn-jion">Jion <MdKeyboardArrowRight className="md_icon" /></button>
+              <div className="col mb-4 mb-md-0 mt-3 d-md-flex pt-4 abs">
+                <div className="col ">
+                  <h3>JOIN OUR TEAM</h3>
+                  <span>jobs@patagonian.com</span>
+                </div>
+                <div className="col ">
+                  <h3>CONTACT US</h3>
+                  <span>jobs@patagonian.com</span>
+                </div>
+              </div>
+            </div>
+
+          </div>
+
+
+        </div>
+      </footer>
+      {/* <footer className="py-5">
           <div className="row p-5">
             <div className="col-md-3 col-sm-6   ComInfo">
               <h5>OUR SERVICES</h5>
@@ -117,10 +116,10 @@ const Footer = () => {
           </div>
          
         </footer> */}
-      
 
 
-   
+
+
 
 
 
@@ -201,16 +200,16 @@ const Footer = () => {
               </div>
             </div>
           </div>
-          
+
 
 
         </div>
       </div>
       <div className="container col-md-11 col-sm-12  text-center d-md-flex justify-content-between  ">
-            <h4 >Brains Pk</h4>
-            <p>&copy; 2023 Company, Inc. All rights reserved.</p>
-    
-          </div>
+        <h4 >Brains Pk</h4>
+        <p>&copy; 2023 Company, Inc. All rights reserved.</p>
+
+      </div>
     </>
   )
 };
