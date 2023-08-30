8b71899a046ba6b2eb214576c47744c061aa402b
proabdulbasit.me <proabdulbasit.me@gmail.com>
2023-08-30 12:59:59 +0500

    wordpress data

--- src/Componentss/Blog/Blog.js
+++ src/Componentss/Blog/Blog.js
@@ -1,76 +1,131 @@
-import React, { useEffect, useState } from "react";
-import { Helmet } from "react-helmet";
-import { Link } from "react-router-dom";
-import "./Blog.css";
+// import React, { useEffect, useState } from "react";
+// import { Helmet } from "react-helmet";
+// import { Link } from "react-router-dom";
+// import "./Blog.css";
 
-const Blog = ({ blogs }) => {
-  console.log("Blog Object :", blogs);
-  console.log(blogs);
+// const Blog = ({ blogs }) => {
+//   console.log("Blog Object :", blogs);
+//   console.log(blogs);
 
-  const [firstImage, setFirstImage] = useState("");
+//   const [firstImage, setFirstImage] = useState("");
+//   useEffect(() => {
+//     handle();
+//   }, []);
+//   const handle = () => {
+//     // setFirstImage(blogs.data[0]?.attributes?.coverImg.data?.attributes.url);
+//   };
+
+//   return (
+//     <>
+//       <Helmet>
+//         <title>Blog - Techcreator Software House</title>
+//         <meta
+//           name="description"
+//           content="Looking for reliable end-to-end solutions? Look no further. Since 2001, we've been providing customized services to meet our clients' unique needs."
+//         />
+//         <meta
+//           name="keywords"
+//           content="techcreator, techcreatorsofwarehouse, sofwarehouse, appsdevelopment, softwareindustry, swabisoftwarehouse, techcreatorabout"
+//         />
+//         <link rel="canonical" href="/blog" />
+//       </Helmet>
+//       <div className="main_class">
+//         <Helmet>
+//           <title>Blog - Techcreator Software Company</title>
+//           <meta
+//             name="description"
+//             content="We have been providing quality services since 2001. We provide our clients with complete end-to-end solutions. We offer customized solutions according to their needs."
+//           />
+//           <meta
+//             name="keywords"
+//             content="techcreator, techcreatorsofwarehouse, sofwarehouse, appsdevelopment, softwareindustry, swabisoftwarehouse, techcreatorabout"
+//           />
+//           <link rel="canonical" href="techcreator.co/blog" />
+//         </Helmet>
+
+//         <div className="class-main w-full bg-[#f9f9f9] py-[50px] mt-10">
+//           <div className="class_b max-w-[1240px] mx-auto">
+//             <div className=" class_c grid lg:grid-cols-3 md:grid-cols-2 sm:grid-cols-2 ss:grid-cols-1 gap-8 px-4 text-black">
+//               {blogs.data.map((blog) => (
+//                 <Link to={`/blogc/${blog.id}`}>
+//                   <div className=" class_d bg-white rounded-xl overflow-hidden drop-shadow-md">
+//                     <img
+//                       className="image h-56 w-full object-cover"
+//                       // src={`http://localhost:1337${blog.attributes?.coverImg.data?.attributes.url}`}
+//                       src={blog.attributes.coverImg.data.attributes.url}
+//                     />
+//                     <div className=" class-e p-8">
+//                       <h3 className="class-f font-bold text-2xl my-3">
+//                         {blog.attributes.blogTitle}
+//                       </h3>
+//                       <p className=" class-g text-gray-600 text-base">
+//                         {blog.attributes.blogDesc}
+//                       </p>
+//                     </div>
+//                   </div>
+//                 </Link>
+//               ))}
+//             </div>
+//           </div>
+//         </div>
+//       </div>
+//     </>
+//   );
+// };
+
+// export default Blog;
+// import './Blog.css'; // Make sure to import your CSS file
+
+// Blog.js
+// Blog.js
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+import { Link } from 'react-router-dom';
+import './Blog.css'; // For Blog component
+function Blog() {
+  const [posts, setPosts] = useState([]);
+
   useEffect(() => {
-    handle();
+    async function fetchPosts() {
+      try {
+        const response = await axios.get('https://techcreator.basit.agency/wp-json/wp/v2/posts');
+        console.log("lsdf", response)
+        setPosts(response.data);
+      } catch (error) {
+        console.error('Error fetching posts:', error);
+      }
+    }
+
+    fetchPosts();
   }, []);
-  const handle = () => {
-    // setFirstImage(blogs.data[0]?.attributes?.coverImg.data?.attributes.url);
-  };
 
   return (
-    <>
-      <Helmet>
-        <title>Blog - Techcreator Software House</title>
-        <meta
-          name="description"
-          content="Looking for reliable end-to-end solutions? Look no further. Since 2001, we've been providing customized services to meet our clients' unique needs."
-        />
-        <meta
-          name="keywords"
-          content="techcreator, techcreatorsofwarehouse, sofwarehouse, appsdevelopment, softwareindustry, swabisoftwarehouse, techcreatorabout"
-        />
-        <link rel="canonical" href="/blog" />
-      </Helmet>
-      <div className="main_class">
-        <Helmet>
-          <title>Blog - Techcreator Software Company</title>
-          <meta
-            name="description"
-            content="We have been providing quality services since 2001. We provide our clients with complete end-to-end solutions. We offer customized solutions according to their needs."
-          />
-          <meta
-            name="keywords"
-            content="techcreator, techcreatorsofwarehouse, sofwarehouse, appsdevelopment, softwareindustry, swabisoftwarehouse, techcreatorabout"
-          />
-          <link rel="canonical" href="techcreator.co/blog" />
-        </Helmet>
-
-        <div className="class-main w-full bg-[#f9f9f9] py-[50px] mt-10">
-          <div className="class_b max-w-[1240px] mx-auto">
-            <div className=" class_c grid lg:grid-cols-3 md:grid-cols-2 sm:grid-cols-2 ss:grid-cols-1 gap-8 px-4 text-black">
-              {blogs.data.map((blog) => (
-                <Link to={`/blogc/${blog.id}`}>
-                  <div className=" class_d bg-white rounded-xl overflow-hidden drop-shadow-md">
-                    <img
-                      className="image h-56 w-full object-cover"
-                      // src={`http://localhost:1337${blog.attributes?.coverImg.data?.attributes.url}`}
-                      src={blog.attributes.coverImg.data.attributes.url}
-                    />
-                    <div className=" class-e p-8">
-                      <h3 className="class-f font-bold text-2xl my-3">
-                        {blog.attributes.blogTitle}
-                      </h3>
-                      <p className=" class-g text-gray-600 text-base">
-                        {blog.attributes.blogDesc}
-                      </p>
-                    </div>
-                  </div>
-                </Link>
-              ))}
-            </div>
-          </div>
-        </div>
+    <div className="blog-container">
+      <div className='heading_class'>
+      <h1>Blog Posts</h1>
       </div>
-    </>
+      <div className="blog-cards-container">
+        {posts.map(post => (
+          <Link to={`/post/${post.slug}`} key={post.id} className="blog-card-link">
+            <div className="blog-card">
+              <div className="blog-card-image">
+                {/* <img src={post.featured_media} alt={post.title.rendered} /> */}
+              </div>
+              <div className="blog-card-content">
+                <h2 className="blog-card-title">{post.title.rendered}</h2>
+                <p className="blog-card-excerpt">
+                  {post.excerpt.rendered.replace(/<\/?[^>]+(>|$)/g, "").substring(0, 100)}...
+                </p>
+              </div>
+            </div>
+          </Link>
+        ))}
+      </div>
+    </div>
   );
-};
+}
 
 export default Blog;
+
+
+
--- src/Router.js
+++ src/Router.js
@@ -1,5 +1,6 @@
+import React from "react";
+import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
 import TopNav from "./Navbar/TopNav";
-import { HashRouter, Routes, Route, BrowserRouter } from "react-router-dom";
 import Home from "./Componentss/Home/Home";
 import About from "./Componentss/About/About";
 import Services from "./Componentss/Our Services/Services";
@@ -9,33 +10,46 @@ import Blog from "./Componentss/Blog/Blog";
 import Portfolio from "./Componentss/Portfolio/portfolio";
 import BlogContentPage from "./Componentss/BlogContentPage/BlogContentPage";
 import useFetch from "../src/hooks/useFetcgh";
-// import { BACKEND_URL, BACKEND_URL2 } from "./helper";
+import Post from "./Componentss/Post/Post";
+
 const RouterPage = () => {
   let { loading, data, error } = useFetch(
     `${process.env.REACT_APP_STRAPI_URL}/api/blogs?populate=*`
   );
   if (loading) return <p>Loading...</p>;
-  // if (error) return <p>Error!</p>;
-  console.log("data :", data);
+
   return (
-    <>
-      <BrowserRouter>
-        <Scrolltoup />
-        <TopNav />
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/about" element={<About />} />
-          <Route path="/services" element={<Services />} />
-          <Route path="/contact" element={<Contact />} />
-          <Route path="/portfolio" element={<Portfolio/>} />
-          <Route path="/blog" element={<Blog blogs={data ? data : ""} />} />
-          <Route
-            path="/blogc/:id"
-            element={<BlogContentPage blogs={data ? data : ""} />}
-          />
-        </Routes>
-      </BrowserRouter>
-    </>
+    <BrowserRouter>
+      <Scrolltoup />
+      <TopNav />
+      {/* Frontity Menu Link */}
+      <nav>
+        <ul>
+          <li>
+            {/* Use Frontity's route for Home */}
+            <Link to="/">new (Frontity)</Link>
+          </li>
+          {/* Add more Frontity menu links here */}
+        </ul>
+      </nav>
+
+      <Routes>
+        {/* React Router Routes */}
+        <Route path="/" element={<Home />} /> {/* Frontity's Home route */}
+        <Route path="/about" element={<About />} />
+        <Route path="/services" element={<Services />} />
+        <Route path="/contact" element={<Contact />} />
+        <Route path="/portfolio" element={<Portfolio />} />
+        <Route path="/post/:slug" element={<Post />} />
+        {/* <Route path="/blog" element={<Blog blogs={data ? data : ""} />} /> */}
+        <Route path="/blog" element={<Blog />} />
+        {/* <Route
+          path="/blogc/:id"
+          element={<BlogContentPage blogs={data ? data : ""} />}
+        /> */}
+      </Routes>
+    </BrowserRouter>
   );
 };
+
 export default RouterPage;
